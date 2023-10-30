4905af9143181ef39d7ab84582e95f19c02baab9
itsnotgunnar <gunnarfranek@gmail.com>
2023-10-30 14:42:51 +0000

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,18 @@ llm_config = {
     "temperature": 0
 }
 
+# User Proxy Agent
+user_proxy = autogen.UserProxyAgent(
+    name="user_proxy",
+    human_input_mode="NEVER",
+    max_consecutive_auto_reply=10,
+    is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
+    code_execution_config={"work_dir": "web"},
+    llm_config=llm_config,
+    system_message="""Reply TERMINATE if the task has been solved at full satisfaction.
+    Otherwise, reply CONTINUE, or the reason why the task is not solved yet."""
+)
+
 # Initialize agents for each role with a system message
 def initialize_agent(name, role_description):
     return autogen.AssistantAgent(
